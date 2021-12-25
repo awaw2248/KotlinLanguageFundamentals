89c249bf1b4930737b2ec9c361e1565ee3390e59
Ahmed Al Hassan <aaa.11.26@outlook.com>
2021-12-25 12:01:42 +0300

    Interface.kt

--- src/main/kotlin/kotlin102ObjectOrientedProgrammimg/Interface.kt
+++ src/main/kotlin/kotlin102ObjectOrientedProgrammimg/Interface.kt
@@ -2,20 +2,27 @@ package kotlin102ObjectOrientedProgrammimg
 
 interface Animal{
 
-    fun move()
-    fun eat()
+    fun move(): String
+    fun eat(): String
 
-    fun breath(): Boolean{
-        return true
+    fun breath(): String{
+        return "yes"
     }
 
 }
-class Dog(): Animal{
-    override fun move() {
+class Dog(): Animal {
+    override fun move(): String {
+        return "Legs"
     }
 
-    override fun eat() {
-        TODO("Not yet implemented")
+    override fun eat(): String {
+        return "steak"
     }
+}
 
+fun main() {
+    val dog= Dog()
+    println(dog.breath())     // doing an instance from [breach()] is possible without overriding anything, opposite to the other functions, [eat()][move()]
+    println(dog.eat())
+    println(dog.move())
 }
